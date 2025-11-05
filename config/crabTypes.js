97f6ff62ec61105fe6e93ac114e1d7675f3b3a46
Tyler <184896961+reallytyler@users.noreply.github.com>
2025-11-05 17:09:07 +0600

    Create crabTypes.js

---
+++ config/crabTypes.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        name: "Red Crab",
+        image: "https://media.istockphoto.com/id/544453032/photo/crab-close-up-cuba.jpg",
+        rarity: "common"
+    },
+    {
+        name: "Blue Crab", 
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Sally_Lightfoot_Crab_2019.jpg/1200px-Sally_Lightfoot_Crab_2019.jpg",
+        rarity: "uncommon"
+    },
+    {
+        name: "Hermit Crab",
+        image: "https://plus.unsplash.com/premium_photo-1667864262393-b5319164c532",
+        rarity: "common"
+    },
+    {
+        name: "King Crab",
+        image: "https://images.unsplash.com/photo-1580841129862-bc2a2d113c45",
+        rarity: "rare"
+    },
+    {
+        name: "Spider Crab",
+        image: "https://images.unsplash.com/photo-1527681192512-bca34fd580bb",
+        rarity: "epic"
+    },
+    {
+        name: "Coconut Crab",
+        image: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b",
+        rarity: "legendary"
+    },
+    {
+        name: "Fiddler Crab",
+        image: "https://images.unsplash.com/photo-1558583057-958fd46d5e1a",
+        rarity: "uncommon"
+    },
+    {
+        name: "Ghost Crab",
+        image: "https://images.unsplash.com/photo-1559005394-1b41f44c4d95",
+        rarity: "rare"
+    },
+    {
+        name: "Horseshoe Crab",
+        image: "https://images.unsplash.com/photo-1559005337-8d7cdcb78da1",
+        rarity: "epic"
+    },
+    {
+        name: "Porcelain Crab",
+        image: "https://images.unsplash.com/photo-1559005336-4e1e10c84c6a",
+        rarity: "legendary"
+    }
+];
